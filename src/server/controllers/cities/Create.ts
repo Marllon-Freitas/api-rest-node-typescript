cbf96468caa373136925c40a19595f3db602c1ba
Marllon-Freitas <marllonfreitas64@gmail.com>
2023-03-24 23:14:48 -0300

    feat/initializing information validation with yup

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,12 +1,25 @@
 import { Request, Response } from "express";
 import StatusCodes from "http-status-codes";
+import * as yup from "yup"
 
 interface City {
-  name: string;
+  city_name: string;
 }
 
-export const create = (req: Request<{}, {}, City>, res: Response) => {
-  const data: City = req.body;
-  console.log(data);
-  return res.status(StatusCodes.CREATED).send("Created");
+const cityBodyValidation: yup.Schema<City> = yup.object().shape({
+  city_name: yup.string().required().min(3),
+});
+
+export const create = async (req: Request<{}, {}, City>, res: Response) => {
+  try {
+    await cityBodyValidation.validate(req.body);
+    return res.status(StatusCodes.OK).send("OK");
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      erros: {
+        default: yupError.message,
+      }
+    });
+  }
 };
