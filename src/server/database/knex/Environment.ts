c36bb4c9637e7aa2dcc398edcccda34bedc4c5d0
Marllon-Freitas <marllonfreitas64@gmail.com>
2023-03-29 13:26:47 -0300

    feat/starting Knex and sqlite settings

---
+++ src/server/database/knex/Environment.ts
@@ -0,0 +1,35 @@
+import { Knex } from "knex";
+import path from "path";
+
+export const development: Knex.Config = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "..",
+      "database.sqlite"
+    ),
+  },
+  migrations: {
+    directory: path.resolve(__dirname, "..", "migrations"),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, "..", "seeds"),
+  },
+  // sqlite configuration to enable foreign keys
+  pool: {
+    afterCreate: (conn: any, done: Function) => {
+      conn.run("PRAGMA foreign_keys = ON", done);
+      done();
+    },
+  },
+};
+
+export const test = {
+  ...development,
+  connection: ":memory:",
+};
---
+++ src/server/database/knex/index.ts
@@ -0,0 +1,18 @@
+import { knex } from "knex";
+
+import { test, development } from "./Environment";
+
+const getEnvironment = () => {
+  switch (process.env.NODE_ENV) {
+    case "test":
+      return test;
+    case "development":
+      return development;
+    default:
+      return development;
+  }
+};
+
+export const Knex = knex(
+  process.env.NODE_ENV === "test" ? test : development
+);
